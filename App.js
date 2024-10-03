fa1e168ae6a1cff58bfe4f49dfa669dc3bbc5be7
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2024-10-02 22:17:16 -0300

    att

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, Switch, Button, TextInput } from "react-native";
+import { View, StyleSheet, Text, Switch, Button, TextInput, Alert } from "react-native";
+import Slider from "@react-native-community/slider";
 
 import { Picker } from "@react-native-picker/picker";
 
@@ -8,36 +9,62 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
+            valor: 0,
             sexo:0,
             sexos: [
-                {key: 1, nome: 'Masculino'},
-                {key: 2, nome: 'Feminino'},
+                {key: 0, nome: 'Masculino'},
+                {key: 1, nome: 'Feminino'},
             ]
         };
+        this.pegaNome = this.pegaNome.bind(this);
+        this.ativarConta = this.ativarConta.bind(this);
     };
 
+    pegaNome(texto) {
+        if (texto.length > 0) {
+          this.setState({ nome: texto });
+        } else {
+          this.setState({ nome: "" });
+        }
+      }
+
+    ativarConta() {
+        const { nome, status, valor, sexo, sexos } = this.state;
+        const sexoNome = sexos.find((v) => v.key == sexo).nome;
+        const statusNome = status ? "Casado" : "Solteiro";
+     
+        Alert.alert(
+          "Sua conta foi criada com sucesso!",
+          `Nome: ${nome}\nSexo: ${sexoNome}\nLimite: $${valor.toFixed(
+            1
+          )}\nStatus: ${statusNome}`
+        );
+      }
+     
+
     render(){
+        let sexosItem = this.state.sexos.map( (v) => {
+            return <Picker.Item key={v.key} value={v.key} label={v.nome} />
+        })
         return(
           <View style={styles.container}>
             <TextInput
             style={styles.input}
             placeholder="Digite seu Nome"
             underlineColorAndroid="transparent"
-            onChangeText={ (texto) => this.setState({input: texto}) }
+            onChangeText={ this.pegaNome}
             />
-        <View style={styles.container}>
-            <Text style={styles.logo}>Informe seu sexo:</Text>
-        <Picker 
-        selectedValue={this.state.sexo} 
-        onValueChange={ (itemValue, itemIndex) => this.setState({sexo: itemValue}) }
+
+        <Text style={{ textAlign: "center", fontSize: 30 }}>Informe seu sexo:</Text>
+        <Picker selectedValue={this.state.sexo} onValueChange={ (itemValue) => this.setState({sexo: itemValue}) }
         >
          {sexosItem}
         </Picker>
-        </View>
 
-        <View style={styles.container}>
-            <Text style={styles.logo}>Escolha o seu limite:</Text>
-        </View>
+
+
+        <Text style={{ textAlign: "center", fontSize: 30 }}>Escolha o seu limite:</Text>
+
         <Slider
           minimumValue={0}
           maximumValue={10000}
@@ -48,6 +75,9 @@ class App extends Component {
           minimumTrackTintColor="#00FF00"
           maximumTrackTintColor="#FF0000"
         />
+        <Text style={{ textAlign: "center", fontSize: 30 }}>
+          Limite da conta: ${this.state.valor.toFixed(1)}
+        </Text>
 
         <Switch
           value={this.state.status}
@@ -60,15 +90,30 @@ class App extends Component {
           {this.state.status ? "Casado" : "Solteiro"}
         </Text>
     
-        <Button title="Criar Conta" onPress={this.entrar} />
+        <Button title="Criar Conta" onPress={this.ativarConta} />
     
-            <Text style={styles.texto}>{this.state.nome}</Text>
-    
-          
     </View>
 
         );
     };
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    input:{
+      height: 45,
+      borderWidth: 1,
+      borderColor: '#222',
+      margin: 10,
+      fontSize: 20,
+      padding: 10,
+    },
+    texto:{
+      textAlign: 'center',
+      fontSize: 25
+    }
+  });
 
 export default App;
