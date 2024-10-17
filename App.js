17148aa685f7f7f6bb5e932a3ccea63867b770a3
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2024-10-16 21:36:09 -0300

    att

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { View, StyleSheet, Text, SafeAreaView } from "react-native";
 
-import Cesta from "./src/Telas/Cesta";
+import Cesta from "./src/telas/Cesta";
+import Mock from "./src/Mocks/cesta";
 
 export default function App() {
     return (
         <SafeAreaView>
             <StatusBar />
-            <Cesta />
+            <Cesta {...Mock} />
         </SafeAreaView>
     );
 }
